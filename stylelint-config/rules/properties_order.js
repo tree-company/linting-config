1b68e75a25b5dcd62b81fad925f73fbbc701e82e
Robbe Van Petegem <robbe@vanpetegem.me>
2023-05-08 15:43:14 +0200

    Setup internal linting

--- stylelint-config/rules/properties_order.js
+++ stylelint-config/rules/properties_order.js
@@ -23,10 +23,13 @@ const ruleIsArray = (rules) => Array.isArray(rules[0]);
 const mergeGroups = (groups) => {
   const groupRules = [];
 
-  for (const [groupName, rules] of Object.entries(groups))
-    if (ruleIsArray(rules))
+  for (const [groupName, rules] of Object.entries(groups)) {
+    if (ruleIsArray(rules)) {
       groupRules.push(...rules.map((props) => createGroup(groupName, props)));
-    else groupRules.push(createGroup(groupName, rules));
+    } else {
+      groupRules.push(createGroup(groupName, rules));
+    }
+  }
 
   return groupRules;
 };
