46a0f315e378beed3083ce6b2d03a12193c58d5c
Robbe Van Petegem <git@robbevp.be>
2025-04-18 17:11:34 +0200

    Allow global modifiers (#170)

--- stylelint-config/rules/selector_class_pattern.js
+++ stylelint-config/rules/selector_class_pattern.js
@@ -1,6 +1,18 @@
 module.exports = {
   BEMPattern: [
-    /^[a-z]+(:)?[a-z]+-?[a-z]*(__[0-9a-z]+-?[0-9a-z]*)?(--[0-9a-z]+-?[a-z]*)?$/,
+    /**
+     * This allows BEM classes with global modifiers
+     * ```css
+     * .block {}
+     * .some-block {}
+     * .block--modifier {}
+     * .block__element {}
+     * .block__some-element {}
+     * .block__element--modifier {}
+     * .--global-modifier {}
+     * ```
+     */
+    /^([a-z]+(:)?[a-z]+-?[a-z]*)?(__[0-9a-z]+-?[0-9a-z]*)?(--[0-9a-z]+-?[a-z]*)?$/,
     {
       resolveNestedSelectors: true,
       message: "Expected class selector to be BEM-style",
